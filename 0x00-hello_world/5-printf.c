6be344bfbf2b61483c47b2313527431e9b474c1d
SaniBrian <sanibrian19@gmail.com>
2023-03-16 15:05:29 -0700

    Added a newline character

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char my_string[] = "with proper grammar, but the outcome is a piece of art,";
+char my_string[] = "with proper grammar, but the outcome is a piece of art,\n";
 printf("%s", my_string);
 return (0);
 }
