8cb5ff5e635f01b394337ec9628c3929f47577ae
SaniBrian <sanibrian19@gmail.com>
2023-03-16 14:44:52 -0700

    C program using puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char my_string[] = "Programming is like building a multilingual puzzle";
+puts(my_string); /*Writing the string to output stream*/
+return (0);
+}
