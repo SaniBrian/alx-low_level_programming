c6d9670b192aef76bbfc95793e7966e377c76ca6
SaniBrian <sanibrian19@gmail.com>
2023-03-16 17:01:55 -0700

    Printing size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+int i;
+long int a;
+long long int b;
+float f;
+
+printf("Size of a char: %c", (unsigned char) sizeof(c));
+printf("bytes(s)\n");
+printf("Size of a int: %d", (unsigned int)sizeof(i));
+printf("bytes(s)\n");
+printf("Size of a long int: %lu", sizeof(a));
+printf("bytes(s)\n");
+printf("Size of a long long int: %llu", (unsigned long long int)sizeof(b));
+printf("bytes(s)\n");
+printf("Size of a float: %lu", (unsigned long) sizeof(f));
+printf("bytes(s)\n");
+return (0);
+}
