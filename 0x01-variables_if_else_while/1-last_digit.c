ae06b0b87604e1b3da0b1f2a46821ebe7768ecee
SaniBrian <sanibrian19@gmail.com>
2023-03-17 12:03:44 -0700

    Used absolute function to get the absolute value of the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d ", n, n % 10);
+	printf("Last digit of %d is %d ", n, abs(n % 10));
 	if (n > 5)
 	{
 		printf("and is greater than 5\n");
@@ -21,7 +21,7 @@ int main(void)
 	{
 		printf("and is 0\n");
 	}
-	if (n < 6 && n == 0)
+	if (n < 6 && n != 0)
 		printf("and is less than and not 0\n");
 	return (0);
 }
