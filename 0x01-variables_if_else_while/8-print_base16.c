ac37c5c260cc36805ac91549a3179b4a72f39c20
SaniBrian <sanibrian19@gmail.com>
2023-03-19 04:09:55 -0700

    Printing the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i = '0';
+	int alph = 'a';
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	while (alph < 'f')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+}
