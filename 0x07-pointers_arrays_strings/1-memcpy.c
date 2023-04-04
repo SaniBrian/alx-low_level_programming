fc6f13dc254b2d1ce7ec15eb6ae40cbfa59b0be7
SaniBrian <sanibrian19@gmail.com>
2023-04-03 18:40:48 -0700

    Fixed the fucntion description

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- *_mempy - copies memory area
+ *_memcpy - copies memory area
  *@dest: memory area destinstion
  *@src: memory area source
  *@n: bytes from memory area
+ *Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int j;
-	j = 0;
 
+	j = 0;
 	while (j < n)
 	{
 		*(dest + j) =  *(src + j);
