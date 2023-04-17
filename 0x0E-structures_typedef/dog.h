2f6345c3c4543ff2c5e920320b7f9448a4388272
SaniBrian <sanibrian19@gmail.com>
2023-04-17 10:00:51 -0700

    Documentation for new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,16 @@
 #ifndef main_h
 #define main_h
-
+/**
+ *struct dog - short description
+ *@name: first member
+ *@age: second member
+ *@owner: third member
+ */
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
