f9480ee7312b2dd6bb0cf842601c648d97b2154c
SaniBrian <sanibrian19@gmail.com>
2023-04-30 16:56:22 -0700

    Task 9 update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp->n = n;
 	current = *head;
 
-	for (i=0; i < idx - 1; i++)
+	for (i = 0; i < idx - 1; i++)
 	{
 		if (current != NULL)
 			current = current->next;
