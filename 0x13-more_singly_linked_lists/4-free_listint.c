c23364434af441b930479ae2516f88091bcd32c3
SaniBrian <sanibrian19@gmail.com>
2023-04-30 13:00:57 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ *free_listint - frees the linked list
+ *@head: head of the linked list
+ *Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+	while((current = head) != NULL)
+	{
+		head = head->next;
+		free(current);
+	}
+}
