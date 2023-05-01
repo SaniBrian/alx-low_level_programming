03139220053277fb81e303592ddd8c9c0fbbc4c5
SaniBrian <sanibrian19@gmail.com>
2023-05-01 00:31:23 -0700

    Updated case of 0 index  for task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 	}
 
-       	temp->next = current->next;
+	temp->next = current->next;
 	current->next = temp;
 
 	return (temp);
