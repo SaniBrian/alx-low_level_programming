50de60fbe09104e26698d0421873e260d99b8bb7
SaniBrian <sanibrian19@gmail.com>
2023-04-30 23:51:04 -0700

    Updated task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	temp->n = n;
+
+	if (*head == NULL)
+		return (NULL);
+
 	if (idx == 0)
 	{
 		*head = temp;
