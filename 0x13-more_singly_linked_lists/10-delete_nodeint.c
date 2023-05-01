2364226d934f048ca83ad71ddae2eda4229480a5
SaniBrian <sanibrian19@gmail.com>
2023-05-01 02:37:00 -0700

    Update for th conditon current is equal to NULL

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,6 +29,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = current->next;
 	}
 
+	if (current == NULL)
+		return (1);
+
 	temp = current->next->next;
 	free(current->next);
 	current->next = temp;
